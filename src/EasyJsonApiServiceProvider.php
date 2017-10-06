fbc5889bcfe0f9e8312645f44ed2507d33f438f6
NgocTP <phuongngoc@gmail.com>
2017-10-06 09:53:58 +0700

    Init EasyJsonApi

---
+++ src/EasyJsonApiServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace NgocTP\EasyJsonApi;
+
+
+class EasyJsonApiServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+}
---
+++ src/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+if (! function_exists('jsonapi')) {
+    /**
+     * Respond JSONAPI format
+     *
+     * @return string
+     */
+    function jsonapi()
+    {
+        // TODO make jsonapi response
+    }
+}
